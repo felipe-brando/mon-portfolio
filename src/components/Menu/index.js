ffdc892f1b34952df4e1aa5777cd18c3f5ba1f61
felipe-brando <f.brandotalero@gmail.com>
2021-10-19 18:04:38 +0200

    adding icons on sidebar menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,5 @@
 import logo from '../../assets/logo-small.png'; 
-
+import { Home, Layout, User, Mail  } from 'react-feather';
 import './style.scss';
 
 const Menu = () => {
@@ -9,10 +9,10 @@ const Menu = () => {
       <img src={logo} className="logo" alt="logo" />
       <nav className="main-nav">
         <ul className="main-nav__item">
-          <li className="main-nav__item"><a href="#">Accueil</a></li>
-          <li className="main-nav__item"><a href="#">Portfolio</a></li>
-          <li className="main-nav__item"><a href="#">A propo</a></li>
-          <li className="main-nav__item"><a href="#">Contact</a></li>
+          <li className="main-nav__item"><a href="#"><Home />Accueil</a></li>
+          <li className="main-nav__item"><a href="#"><Layout />Portfolio</a></li>
+          <li className="main-nav__item"><a href="#"><User />A propo</a></li>
+          <li className="main-nav__item"><a href="#"><Mail />Contact</a></li>
         </ul>
       </nav>
     </header>
