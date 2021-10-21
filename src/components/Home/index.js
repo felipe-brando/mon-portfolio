0a0a6122d86e62106d215787400fd218fb8dbb59
felipe-brando <f.brandotalero@gmail.com>
2021-10-21 18:22:56 +0200

    adding styles on introduction bloc

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,11 +5,14 @@ import './style.scss';
 const Home = () => {
     return (
         <Page>
-            <Avatar className="avatar" />
-            <section className="intro">
-            <h1>Title</h1>
-            <p>Texte</p>
-            </section>
+            <div className="intro">
+                <Avatar />
+                <section className="intro__content">
+                    <p className="intro__hello">Bonjour ! Je suis</p>
+                    <p className="intro__name">Felipe Brando</p>
+                    <h1 className="intro__content">Développeur web fullstack & Chef de projet digital <spam>avec une touche de créativité</spam></h1>
+                </section>
+            </div>
         </Page>
     );
 };
