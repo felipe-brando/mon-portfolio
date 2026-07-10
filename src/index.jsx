149ef97294792b15c6a0e298c5e1f24dc49bca98
Felipe Brando <felipe@macbook-pro-de-felipe-1.home>
2026-07-10 15:54:03 +0200

    Migration CRA vers Vite reussie

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './components/App/App';
-import store from './store/index.js';
+import store from './store';
 import './styles/index.scss';
 
 import ReactGA from 'react-ga';
